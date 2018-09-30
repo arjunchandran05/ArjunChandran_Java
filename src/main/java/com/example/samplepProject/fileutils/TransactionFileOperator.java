e7ab7e2a29e40c7a683c0dfaed54384b91388817
Arjun Chandran <arjun.acer@hotmail.com>
2018-09-30 22:16:17 +0530

    Commiting working solution.

--- src/main/java/com/example/samplepProject/fileutils/TransactionFileOperator.java
+++ src/main/java/com/example/samplepProject/fileutils/TransactionFileOperator.java
@@ -13,6 +13,7 @@ import java.util.List;
 import java.util.Map;
 
 import org.apache.commons.io.IOUtils;
+import org.junit.experimental.theories.Theories;
 
 import com.example.sampleProject.pojo.PositionInfo;
 import com.example.sampleProject.pojo.Transaction;
@@ -22,23 +23,18 @@ import com.opencsv.CSVReader;
 
 public class TransactionFileOperator {
 
-	private static String inputFilePath = "C:\\Arjun_Chandran\\Personal\\Projects\\transactionProcessor\\src\\main\\resources";
-	
-	public static String getInputFilePath() {
-		return inputFilePath;
-	}
-	public TransactionFileOperator(String inputFilePath) {
-		TransactionFileOperator.inputFilePath = inputFilePath;
-	}
-	
+	private static String inputFilePath;
+	private static String outputFilePath;
+
 	private static Map<String, List<Transaction>> transactionsMap = new HashMap<String, List<Transaction>>();
 	private static List<PositionInfo> listOfInputs;
 
 	public static void main(String args[]) throws IOException {
-		TransactionFileOperator.load(inputFilePath);
+		TransactionFileOperator.load();
 	}
-	public static void load(String filePath) throws IOException {
-		File folder = new File(filePath);
+
+	public static void load() throws IOException {
+		File folder = new File(TransactionFileOperator.getInputFilePath());
 
 		for (final File fileEntry : folder.listFiles()) {
 			if (fileEntry.isFile() && fileEntry.getName().endsWith(".csv")) {
@@ -46,19 +42,19 @@ public class TransactionFileOperator {
 				CSVReader reader = null;
 				try {
 					reader = new CSVReader(new FileReader(fileEntry));
-					String [] nextLine;
+					String[] nextLine;
 					listOfInputs = new ArrayList<PositionInfo>();
-					nextLine = reader.readNext();
+					String [] headerLine = reader.readNext();
 					while ((nextLine = reader.readNext()) != null) {
-					    // nextLine[] is an array of values from the line
-						listOfInputs.add(new PositionInfo(nextLine[0], Long.parseLong(nextLine[1]), nextLine[2], Long.parseLong(nextLine[3])));
-					    System.out.println(nextLine[0] + nextLine[1] + "etc...");
+						// nextLine[] is an array of values from the line
+						listOfInputs.add(new PositionInfo(nextLine[0], Long.parseLong(nextLine[1]), nextLine[2],
+								Long.parseLong(nextLine[3])));
+						System.out.println(nextLine[0] + nextLine[1] + "etc...");
 					}
-				}
-				catch(IOException ex) {
-					System.out.println("IO exception occured while reading csv file at :: " + fileEntry.getAbsolutePath());
-				}
-				finally {
+				} catch (IOException ex) {
+					System.out.println(
+							"IO exception occured while reading csv file at :: " + fileEntry.getAbsolutePath());
+				} finally {
 					if (reader != null) {
 						reader.close();
 					}
@@ -69,62 +65,80 @@ public class TransactionFileOperator {
 				// load transactions
 				try {
 					InputStream is = new FileInputStream(fileEntry);
-		            String jsonTxt = IOUtils.toString(is, "UTF-8");
+					String jsonTxt = IOUtils.toString(is, "UTF-8");
 					ObjectMapper mapper = new ObjectMapper();
 					// mapper.configure(MapperFeature.AUTO_DETECT_FIELDS, true);
-					List<Transaction> listOfTransactions = mapper.readValue(jsonTxt, new TypeReference<List<Transaction>>() {});
+					List<Transaction> listOfTransactions = mapper.readValue(jsonTxt,
+							new TypeReference<List<Transaction>>() {
+							});
 					listOfTransactions.forEach(transaction -> {
 						if (transactionsMap.get(transaction.getInstrument()) != null) {
-							List <Transaction> t = transactionsMap.get(transaction.getInstrument());
+							List<Transaction> t = transactionsMap.get(transaction.getInstrument());
 							t.add(transaction);
 							transactionsMap.put(transaction.getInstrument(), t);
-						}
-						else {
+						} else {
 							List<Transaction> t = new ArrayList<Transaction>();
 							t.add(transaction);
 							transactionsMap.put(transaction.getInstrument(), t);
 						}
 					});
+				} catch (IOException ex) {
+					System.out.println(
+							"IO exception occured while reading json file at :: " + fileEntry.getAbsolutePath());
 				}
-				catch(IOException ex) {
-					System.out.println("IO exception occured while reading json file at :: " + fileEntry.getAbsolutePath());
-				}
-				
+
 			}
 		}
 	}
-	
-	public static void writeObjectAsJSON(List<PositionInfo> list, String filePath) throws IOException {
-		File folder = new File(filePath);
 
-		for (final File fileEntry : folder.listFiles()) {
-			if (fileEntry.isFile() && fileEntry.getName().endsWith(".csv")) {
-				// load input
-				try {
-					final ByteArrayOutputStream out = new ByteArrayOutputStream();
-				    final ObjectMapper mapper = new ObjectMapper();
-				    mapper.writeValue(out, list);
+	public static void writeObjectAsJSON(List<PositionInfo> list) throws IOException {
+		try {
+			final ByteArrayOutputStream out = new ByteArrayOutputStream();
+			final ObjectMapper mapper = new ObjectMapper();
+			mapper.writeValue(out, list);
+			String headers = "Instrument,Account,AccountType,Quantity,Delta \r \n";
+			mapper.writeValue(new File(TransactionFileOperator.getOutputFilePath() + "\\Expected_EndOfDay_Positions.json", headers), list );
+			// mapper.writeValue(new File(TransactionFileOperator.getOutputFilePath() + "\\Expected_EndOfDay_Positions.json"), list );
 
-				    final byte[] data = out.toByteArray();
-				    FileOutputStream stream = new FileOutputStream(filePath);
-				    try {
-				        stream.write(data);
-				    } finally {
-				        stream.close();
-				    }
-				    System.out.println(new String(data));
-				}
-				catch(IOException ex) {
-					System.out.println("IO exception occured while writing json file ");
-				}
+			final byte[] data = out.toByteArray();
+			FileOutputStream stream = new FileOutputStream(TransactionFileOperator.getOutputFilePath());
+			try {
+				stream.write(data);
+			} finally {
+				stream.close();
 			}
+			System.out.println(new String(data));
+		} catch (IOException ex) {
+			System.out.println("IO exception occured while writing json file ");
 		}
 	}
+
 	public static Map<String, List<Transaction>> getTransactionsMap() {
 		return transactionsMap;
 	}
+
 	public static List<PositionInfo> getListOfInputs() {
 		return listOfInputs;
+	}
+
+	public static void setInputFilePath(String inputFilePath) {
+		TransactionFileOperator.inputFilePath = inputFilePath;
+	}
+
+	public static String getInputFilePath() {
+		return inputFilePath;
+	}
+
+	public TransactionFileOperator(String inputFilePath) {
+		TransactionFileOperator.inputFilePath = inputFilePath;
+	}
+
+	public static String getOutputFilePath() {
+		return outputFilePath;
+	}
+
+	public static void setOutputFilePath(String outputFilePath) {
+		TransactionFileOperator.outputFilePath = outputFilePath;
 	}
 
 }
